66c3eee27cf9309622a2c26704fa0d00645a9773
sayalipagar <pagarsayali565@gmail.com>
2022-09-10 13:28:14 +0530

    [sayali] Add:Welcome TO Generics Problem Statement

---
+++ GenericsProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Problem Statement!!!");
+        }
+    }
+}
