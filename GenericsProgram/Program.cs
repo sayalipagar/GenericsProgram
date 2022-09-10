d417e6b0cf3928e31aa53a2d18715f5ad09d447b
sayalipagar <pagarsayali565@gmail.com>
2022-09-10 14:14:14 +0530

    [sayali] Add:UsigGenericPrintArray

--- GenericsProgram/Program.cs
+++ GenericsProgram/Program.cs
@@ -6,13 +6,17 @@
         {
             while (true)
             {
-                Console.WriteLine("\nselect program  \n 1.CreateArray");
+                Console.WriteLine("\nselect program  \n 1.CreateArray\n 2.UsingGenericMethod");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         CreateArrray create = new CreateArrray();
                         create.createinpiut();
+                        break;
+                    case 2:
+                        UsingGeneric generic= new UsingGeneric();
+                        generic.usingGeneric();
                         break;
                 }
             }
---
+++ GenericsProgram/UsingGeneric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    //Generic Class
+     class UsingGeneric
+    {
+        public void usingGeneric()
+        {
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            PrintArr<int>(intArr);
+            PrintArr<double>(doubleArr);
+            PrintArr<char>(charArr);
+            
+        }
+        static void PrintArr<T>(T[] arr)
+        {
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
+    
+
